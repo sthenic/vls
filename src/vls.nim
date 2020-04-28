5ecae0ea0489fda9d75093e5decd7d143db46439
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2020-04-28 20:58:06 +0200

    Use stdin/stdout instead of sockets

--- src/vls.nim
+++ src/vls.nim
@@ -1,66 +1,42 @@
-import net
+import json
+import streams
+import strutils
+
+var ifs = new_file_stream(stdin)
+var ofs = new_file_stream(stdout)
 
 when is_main_module:
-   var socket = new_socket(AF_INET, SOCK_STREAM, IPPROTO_TCP, false)
-   bind_addr(socket, Port(8000))
-   listen(socket)
-   echo "Listening on port 8000"
-
-   var client: Socket
-   var address = ""
    while true:
-      accept_addr(socket, client, address)
-      echo "Client connected from ", address
+      var header = ""
+      if not read_line(ifs, header):
+         echo "Input stream closed."
+         break
 
-      # Collect the client's request.
-      var request_raw = ""
-      var done = false
-      var par_count = 0
-      while true:
-         var data = ""
-         try:
-            data = recv(client, 1024, 1000)
-         except TimeoutError:
-            echo "Timeout while waiting for a complete request."
-            send(client, "Timeout while waiting for a complete request.")
-            close(client)
-            break
-
-         if data == "":
-            echo "Connection closed from ", address
-            break
-
-         for i, c in data:
-            add(request_raw, c)
-            case c
-            of '{':
-               inc(par_count)
-            of '}':
-               dec(par_count)
-               if par_count == 0:
-                  echo "Done"
-                  if i != high(data):
-                     echo "error, there's still data in the TCP buffer"
-                  done = true
-                  break
-            else:
-               discard
-
-         if done:
-            break
-
-      if not done:
-         echo "Failed to collect a valid request"
-         close(client)
+      if len(header) == 0:
+         echo "empty!"
          continue
 
-      echo "Got request data, length ", len(request_raw)
+      if not starts_with(header, "Content-Length: "):
+         echo "error!"
+         continue
 
-      # Process the request.
-      let response = request_raw
+      let length =
+         try:
+            parse_int(substr(header, 16))
+         except ValueError:
+            -1
 
-      # Send the reply.
-      send(client, response)
-      close(client)
+      if length < 0:
+         echo "error invalid length ", length
+         continue
 
-   close(socket)
+      let frame = read_str(ifs, length)
+      let node =
+         try:
+            parse_json(frame)
+         except JsonParsingError:
+            echo "failed JSON parsing"
+            continue
+
+      write_line(ofs, pretty(node))
+
