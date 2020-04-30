914b82ccd51ee3b6b5eaede1035fbd30ab397269
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2020-04-30 23:49:44 +0200

    Start working on the protocol parser

---
+++ src/protocol.nim
@@ -0,0 +1,126 @@
+# This file implements the protocol handling.
+
+import json
+import streams
+import strutils
+
+export json
+
+type
+   Request* = object
+      length: int
+      id*: int
+      md*: string
+      parameters*: JsonNode
+
+   RequestValueError* = object of ValueError
+   RequestIoError* = object of IOError
+
+
+proc new_request_io_error(msg: string, args: varargs[string, `$`]): ref RequestIoError =
+   new result
+   result.msg = format(msg, args)
+
+
+proc new_request_value_error(msg: string, args: varargs[string, `$`]): ref RequestValueError =
+   new result
+   result.msg = format(msg, args)
+
+
+proc parse_headers(s: Stream, r: var Request) =
+   var seen_content_length = false
+
+   const CONTENT_LENGTH = "Content-Length: "
+   const CONTENT_TYPE = "Content-Type: "
+   const CONTENT_TYPE_UTF8 = "application/vscode-jsonrpc; charset=utf-8"
+
+   while true:
+      var header = ""
+      if not read_line(s, header):
+         raise new_request_io_error("Input stream closed unexpectedly.")
+
+      if len(header) == 0:
+         # We're done with parsing headers. The message is expected next.
+         # However, the Content-Length header is mandatory and if we haven't
+         # seen that we have to raise an error.
+         if not seen_content_length:
+            raise new_request_value_error("The request is missing the " &
+                                          "required header field " &
+                                          "'Content-Length'.")
+         else:
+            break
+
+      if starts_with(header, CONTENT_LENGTH):
+         let length = substr(header, len(CONTENT_LENGTH))
+         try:
+            r.length = parse_int(length)
+         except ValueError:
+            raise new_request_value_error("Invalid content length: '$1'.", length)
+
+         seen_content_length = true
+
+      elif starts_with(header, CONTENT_TYPE):
+         let content_type = substr(header, len(CONTENT_TYPE))
+         if content_type != CONTENT_TYPE_UTF8:
+            raise new_request_value_error(
+               "Invalid content type '$1'. Expected '$2'.",
+               content_type, CONTENT_TYPE_UTF8
+            )
+
+      else:
+         raise new_request_value_error("Invalid request header '$1'.", header)
+
+
+proc parse_content(s: Stream, r: var Request) =
+   let content = read_str(s, r.length)
+   let node =
+      try:
+         parse_json(content)
+      except JsonParsingError:
+         raise new_request_value_error("Parsing error when processing JSON content.")
+
+   # Validate JSON object.
+   if node.kind != JObject:
+      raise new_request_value_error("Content is not a JSON object.")
+
+   # Validate JSON RPC version.
+   try:
+      if get_str(node["jsonrpc"]) != "2.0":
+         raise new_request_value_error(
+            "Expected JSON RPC version 2.0, got '$1'.", node["jsonrpc"]
+         )
+   except KeyError:
+      raise new_request_value_error("Expected key 'jsonrpc'.")
+
+   # Handle the request id.
+   try:
+      case node["id"].kind
+      of JInt:
+         r.id = get_int(node["id"])
+      of JString:
+         r.id = parse_int(get_str(node["id"]))
+      else:
+         raise new_request_value_error(
+            "Unexpected type of 'id' field, expected an integer or a string.")
+   except KeyError:
+      raise new_request_value_error("Expected key 'id'.")
+   except ValueError:
+      raise new_request_value_error("Invalid id '$1'.", get_str(node["id"]))
+
+   # Handle the request method.
+   try:
+      if node["method"].kind != JString:
+         raise new_request_value_error("Unexpected type of 'method' field, " &
+                                       "expected a string.")
+      r.md = get_str(node["method"])
+   except KeyError:
+      raise new_request_value_error("Expected key 'method'.")
+
+
+proc get_request*(s: Stream): Request =
+   # Read the header part. Any parse error will raise an exception which should
+   # propagate to the caller and generate an error response.
+   parse_headers(s, result)
+
+   # Read the content part.
+   parse_content(s, result)
--- src/vls.nim
+++ src/vls.nim
@@ -1,42 +1,33 @@
-import json
 import streams
 import strutils
 
-var ifs = new_file_stream(stdin)
-var ofs = new_file_stream(stdout)
+import ./protocol
 
-when is_main_module:
-   while true:
-      var header = ""
-      if not read_line(ifs, header):
-         echo "Input stream closed."
-         break
+const
+   EOK = 0
+   ESTREAM = 1
 
-      if len(header) == 0:
-         echo "empty!"
+var ofs: FileStream
+var ifs: FileStream
+
+try:
+   ofs = new_file_stream(stdout)
+   ifs = new_file_stream(stdin)
+except IOError:
+   quit(-ESTREAM)
+
+
+while true:
+   let req =
+      try:
+         get_request(ifs)
+      except RequestIoError as e:
+         write_line(ofs, format("Error: $1", e.msg))
+         quit(-ESTREAM)
+      except RequestValueError as e:
+         write_line(ofs, format("Error: $1", e.msg))
          continue
 
-      if not starts_with(header, "Content-Length: "):
-         echo "error!"
-         continue
+   write_line(ofs, pretty(req.parameters))
 
-      let length =
-         try:
-            parse_int(substr(header, 16))
-         except ValueError:
-            -1
-
-      if length < 0:
-         echo "error invalid length ", length
-         continue
-
-      let frame = read_str(ifs, length)
-      let node =
-         try:
-            parse_json(frame)
-         except JsonParsingError:
-            echo "failed JSON parsing"
-            continue
-
-      write_line(ofs, pretty(node))
-
+quit(EOK)
