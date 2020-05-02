a1bc607f65bb60e23847cda712dd652a83763fc2
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2020-05-02 15:55:01 +0200

    [server] Draft handling of the 'initialize' request

--- src/server.nim
+++ src/server.nim
@@ -11,6 +11,7 @@ type
    LspServer* = object
       ifs, ofs: Stream
       is_initialized: bool
+      root_uri: string
 
 
 proc open*(s: var LspServer, ifs, ofs : Stream) =
@@ -24,17 +25,49 @@ proc close*(s: var LspServer) =
    close(s.ofs)
 
 
+proc initialize(s: var LspServer, msg: LspMessage) =
+   try:
+      s.root_uri = get_str(msg.parameters["rootUri"])
+
+      var parameters = new_jobject()
+      parameters["serverInfo"] = %*{
+         "name": "vls",
+         # FIXME: Read this from a shared static location.
+         "version": "0.1.0"
+      }
+      parameters["capabilities"] = %*{
+         "textDocument": {
+            "declaration": {
+               "dynamicRegistration": false,
+               "linkSupport": false
+            }
+         }
+      }
+      send_response(s.ofs, new_lsp_response(msg.id, parameters))
+
+   except KeyError as e:
+      send_response(s.ofs, new_lsp_response(msg.id, RPC_PARSE_ERROR, e.msg, nil))
+
+
 proc handle_request(s: var LspServer, msg: LspMessage) =
-   case msg.m
-   of "initialize":
-      s.is_initialized = true
-   else:
-      let str = format("Unsupported method '$1'.", msg.m)
-      send_response(s.ofs, new_lsp_response(msg.id, RPC_METHOD_NOT_FOUND, str, nil))
+   # If the server is not initialized, we only respond to the 'initialize'
+   # request.
+   if not s.is_initialized:
+      if msg.m == "initialize":
+         initialize(s, msg)
+      else:
+         send_response(s.ofs, new_lsp_response(msg.id, RPC_SERVER_NOT_INITIALIZED, "", nil))
+      return
+
+   let str = format("Unsupported method '$1'.", msg.m)
+   send_response(s.ofs, new_lsp_response(msg.id, RPC_METHOD_NOT_FOUND, str, nil))
 
 
 proc handle_notification(s: LspServer, msg: LspMessage) =
-   discard
+   # If the server is not initialized, all notifications should be dropped,
+   # except for the exit notification.
+   if not s.is_initialized:
+      return
 
 
 proc run*(s: var LspServer): int =
@@ -50,6 +83,7 @@ proc run*(s: var LspServer): int =
             send_response(s.ofs, new_lsp_response(0, RPC_PARSE_ERROR, e.msg, nil))
             continue
 
+      # Handle the message.
       case msg.kind
       of MkRequest:
          handle_request(s, msg)
@@ -57,6 +91,5 @@ proc run*(s: var LspServer): int =
          handle_notification(s, msg)
       else:
          send_response(s.ofs, new_lsp_response(msg.id, RPC_INVALID_REQUEST, "", nil))
-         continue
 
    result = EOK
