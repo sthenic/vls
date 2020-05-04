35c94ed2bbf94ce9a05440f99687dcf348081f9b
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2020-05-04 19:52:18 +0200

    [log] Don't use syslog on Windows

--- src/log.nim
+++ src/log.nim
@@ -4,27 +4,12 @@ when not defined(windows):
    proc syslog(priority: cint, msg: cstring) {.importc, header: "<syslog.h>".}
 
 const
-   LOG_EMERG = cint(0)
-   LOG_ALERT = cint(1)
-   LOG_CRIT = cint(2)
    LOG_ERR = cint(3)
    LOG_WARNING = cint(4)
-   LOG_NOTICE = cint(5)
    LOG_INFO = cint(6)
    LOG_DEBUG = cint(7)
 
-   LOG_KERN = cint(0 shl 3)
    LOG_USER = cint(1 shl 3)
-   LOG_MAIL = cint(2 shl 3)
-   LOG_DAEMON = cint(3 shl 3)
-   LOG_AUTH = cint(4 shl 3)
-   LOG_SYSLOG = cint(5 shl 3)
-   LOG_LPR = cint(6 shl 3)
-   LOG_NEWS = cint(7 shl 3)
-   LOG_UUCP = cint(8 shl 3)
-   LOG_CRON = cint(9 shl 3)
-   LOG_AUTHPRIV = cint(10 shl 3)
-   LOG_FTP = cint(11 shl 3)
 
 
 type LogTarget* = enum
@@ -38,31 +23,42 @@ proc set_log_target*(target: LogTarget) =
    log_target = target
 
 
-template write(header, msg: string, args: varargs[string, `$`]) =
-   if log_target == SYSLOG:
-      for line in split_lines(format(msg, args)):
-         syslog(LOG_INFO or LOG_DAEMON, line)
+template write_stderr(header, msg: string, args: varargs[string, `$`]) =
+   let msg_split = split_lines(format(msg, args))
+   write(stderr, header & msg_split[0] & "\n")
+   for i in 1..<len(msg_split):
+      write(stderr, "         " & msg_split[i] & "\n")
+
+
+template write_syslog(level: cint, msg: string, args: varargs[string, `$`]) =
+   for line in split_lines(format(msg, args)):
+      syslog(level or LOG_USER, line)
+
+
+template write(level: cint, header, msg: string, args: varargs[string, `$`]) =
+   when defined(windows):
+      write_stderr(header, msg, args)
    else:
-      let msg_split = split_lines(format(msg, args))
-      write(stderr, header & msg_split[0] & "\n")
-      for i in 1..<len(msg_split):
-         write(stderr, "         " & msg_split[i] & "\n")
+      if log_target == SYSLOG:
+         write_syslog(level, msg, args)
+      else:
+         write_stderr(header, msg, args)
 
 
 template info*(msg: string, args: varargs[string, `$`]) =
-   write("INFO:    ", msg, args)
+   write(LOG_INFO, "INFO:    ", msg, args)
 
 
 template warning*(msg: string, args: varargs[string, `$`]) =
-   write("WARNING: ", msg, args)
+   write(LOG_WARNING, "WARNING: ", msg, args)
 
 
 template error*(msg: string, args: varargs[string, `$`]) =
-   write("ERROR:   ", msg, args)
+   write(LOG_ERR, "ERROR:   ", msg, args)
 
 
 template debug_always*(msg: string, args: varargs[string, `$`]) =
-   write("DEBUG:   ", msg, args)
+   write(LOG_DEBUG, "DEBUG:   ", msg, args)
 
 
 template debug*(msg: string, args: varargs[string, `$`]) =
